8a147e19d5b7e0e389188ff90eb73bade58be5da
kolszewska <k.olszewskaaa@gmail.com>
2021-12-08 13:44:30 +0000

    Fix home test

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'html_pages#home'
   get 'html_pages/help'
   get 'html_pages/about'
+  get 'html_pages/home'
   resources :feedbacks
   resources :users
 end
