86eb4e8e78e178000727e917e600637484115d7e
kolszewska <k.olszewskaaa@gmail.com>
2021-12-08 13:43:28 +0000

    Open home page on root

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
-  get 'html_pages/home'
+  root 'html_pages#home'
   get 'html_pages/help'
   get 'html_pages/about'
   resources :feedbacks
   resources :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'users#index'
 end
