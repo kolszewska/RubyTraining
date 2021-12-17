b643da3490f758d24aa2ebf5270812eb728f920d
kolszewska <k.olszewskaaa@gmail.com>
2021-12-17 13:48:35 +0000

    Add user validation and tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
   has_many :feedbacks
+  validates :name, presence: true, length: { maximum: 60 }
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: URI::MailTo::EMAIL_REGEXP },
+                    uniqueness: { case_sensitive: false }
 end
