a542cbc90e76a920641d9bedf828b19f6bec0a13
kolszewska <k.olszewskaaa@gmail.com>
2022-02-02 14:03:46 +0000

    Add authorisation for edit pages

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -1,9 +1,17 @@
 require 'test_helper'
 
 class SessionsControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = users(:karolina)
+  end
+
   test "should get new" do
     get login_path
     assert_response :success
   end
 
+  test 'should do friendly forwarding' do
+    log_in_as(@user)
+    assert_redirected_to user_url(@user)
+  end
 end
